ee7c1f6706f60a4da8bc992f123d5c1fb3f2f8bd
Moshiur Rahman <unimrgm@gmail.com>
2016-11-20 23:18:01 +0600

    Primary Layout Fixed

---
+++ footer.php
@@ -0,0 +1,7 @@
+		</div> <!-- .content-wrapper -->
+	</main> <!-- .cd-main-content -->
+<script src="js/jquery-2.1.4.js"></script>
+<script src="js/jquery.menu-aim.js"></script>
+<script src="js/main.js"></script> <!-- Resource jQuery -->
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,110 @@
+<?php include 'header.php'; ?>
+<h1>Responsive Sidebar Navigation</h1>
+<table id="data-table-row-grouping" class="display" border="1" cellspacing="0" width="100%" id="searchproduct">
+    <thead>
+        <tr>
+        <th>Name</th>
+        <th>Date</th>
+        <th>Previous Stock</th>
+        <th>Production</th>
+        <th>Return</th>
+        <th>Sold</th>
+        <th>Waste</th>
+        <th>Available</th>
+        </tr>
+    </thead>
+ 
+    <tbody>
+        <tr>
+            <td id="products">Tiger Nixon</td>
+            <td>2011/04/25</td>
+            <td>5000</td>
+            <td>500</td>
+            <td>200</td>
+            <td>800</td>
+            <td>50</td>
+            <td>7200</td>
+        </tr>
+        <tr>
+            <td id="products">Garrett Winters</td>
+            <td>2011/07/25</td>
+            <td>5000</td>
+            <td>500</td>
+            <td>200</td>
+            <td>800</td>
+            <td>50</td>
+            <td>7200</td>
+        </tr>
+        <tr>
+            <td id="products">Ashton Cox</td>
+            <td>2009/01/12</td>
+            <td>5000</td>
+            <td>500</td>
+            <td>200</td>
+            <td>800</td>
+            <td>50</td>
+            <td>7200</td>
+        </tr>
+        <tr>
+            <td id="products">Cedric Kelly</td>
+            <td>2012/03/29</td>
+            <td>5000</td>
+            <td>500</td>
+            <td>200</td>
+            <td>800</td>
+            <td>50</td>
+            <td>7200</td>
+        </tr>
+        <tr>
+            <td id="products">Airi Satou</td>
+            <td>2008/11/28</td>
+           <td>5000</td>
+           <td>500</td>
+           <td>200</td>
+           <td>800</td>
+           <td>50</td>
+           <td>7200</td>
+        </tr>
+        <tr>
+            <td id="products">Brielle Williamson</td>
+            <td>2012/12/02</td>
+            <td>5000</td>
+            <td>500</td>
+            <td>200</td>
+            <td>800</td>
+            <td>50</td>
+            <td>7200</td>
+        </tr>
+        <tr>
+            <td id="products">Herrod Chandler</td>
+            <td>2012/08/06</td>
+           <td>5000</td>
+           <td>500</td>
+           <td>200</td>
+           <td>800</td>
+           <td>50</td>
+           <td>7200</td>
+        </tr>
+        <tr>
+            <td id="products">Herrod Chandler</td>
+            <td>2012/08/06</td>
+           <td>5000</td>
+           <td>500</td>
+           <td>200</td>
+           <td>800</td>
+           <td>50</td>
+           <td>7200</td>
+        </tr>
+        <tr>
+            <td id="products">Herrod Chandler</td>
+            <td>2012/08/06</td>
+           <td>5000</td>
+           <td>500</td>
+           <td>200</td>
+           <td>800</td>
+           <td>50</td>
+           <td>7200</td>
+        </tr>
+    </tbody>
+</table>
+<?php include 'footer.php'; ?>
